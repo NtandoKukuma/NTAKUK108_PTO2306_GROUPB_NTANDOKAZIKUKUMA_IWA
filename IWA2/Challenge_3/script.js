b00735ba7b274c2209e64ce8b9b6da475629dc5a
NtandoKukuma <140637574+NtandoKukuma@users.noreply.github.com>
2023-09-19 09:38:46 +0200

    uploading IWA2

---
+++ IWA2/Challenge_3/script.js
@@ -0,0 +1,12 @@
+// 
+// It is important to show the following message in order to:
+// - assure users
+// - scare hackers
+// - impress investors
+//
+
+console.warn("Security scan starting"); // Use double quotes for the message
+
+/* It is important to let users know when they can close the page  */
+
+console.info('Please wait for the scan to complete before closing the browser.'); // Fixed the closing parenthesis and typo in "console"
